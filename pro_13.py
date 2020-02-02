c085d6a09170cba6a107e786c8a23274203e7f9b
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-02-02 18:34:51 +0900

    I need to learn VFS,DFS,DP and other thing
    I think I must learn that it will help me to solve other problem with ez way

--- pro_13.py
+++ pro_13.py
@@ -1,4 +1,6 @@
-a=list(input())
-print(a)
-b=list(map(int, a))
-print(b)
+num_sum = 0
+for i in range(100):
+    num_input = int(input()[:13])
+    num_sum += num_input
+num_site = list(str(num_sum))[:10]
+print(''.join(num_site))
--- pro_15.py
+++ pro_15.py
@@ -1,1 +1,22 @@
 #i think it is problem about combination. 40 0 and i change 20 0 to 1
+# arr = [0]*40
+# n=len(arr)
+# count = 0
+# for i in range(2**20 -1, 2**40 - 2**(19) + 1):
+#     C=list(bin(i)[2:])
+#     if C.count('1') != 20:
+#         continue
+#     for j in range(n):
+#         if i&(1<<j):
+#             count += 1
+# print(count//20)
+
+# and other idea is checking 1 by 1 root
+N=int(input())
+grid = [[1]*N]
+for i in range(1,N):
+    NX =[1]
+    for j in range(1,N):
+        NX.append(NX[j-1]+grid[i-1][j])
+    grid.append(NX)
+print(grid[N-1][N-1])
