c374e8b115d1d65be442bd5edb241ebcafc0b3dd
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-02-03 23:45:58 +0900

    0203
    I solve many problem. but solve little problem in euler. I think I must learn many thing. but I'll solve euler with little knowledge

---
+++ pro_14.py
@@ -0,0 +1,2 @@
+# I need to learn dfs and dp
+# so i'll solve this problem after I learn that
---
+++ pro_17.py
@@ -0,0 +1,7 @@
+stack = []
+miro =[]
+N=int(input())
+for _ in range(N):
+    miro_garo = list(map(int,input()))
+    miro.append(miro_garo)
+print(miro)
